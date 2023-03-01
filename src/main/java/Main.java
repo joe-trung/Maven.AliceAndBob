b2cfb6491e2a8f5aac6e7ea00762e1406f2da2f1
joe-trung <kieutrungtran@gmail.com>
2023-03-01 16:12:39 -0500

    Done Deal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner inName = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String aName = inName.nextLine();
+        if ((aName.equals("Alice")) || (aName.equals("Bob"))) {
+            System.out.print("Hello " + aName + "! How are you?");
+        }
+        else {
+            System.out.print("Bye");
+        }
+        }
     }
-}
